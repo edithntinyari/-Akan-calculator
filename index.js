a32168cffe58bdff09ca4eeaa57b2073004cc16d
Edithntinyari <edithtinyari25@gmail.com>
2020-02-17 08:00:10 +0300

    add var

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ function giveAkanName () {
     var gender=getGender();
     var date = new Date(year +"/" + month + "/"+day);
     var dayBorn = date.getDay();
-    var akanName;
-    var validate = (year > 0 && (month >0 && month <=12) && (day > 0 && day <= 31));
+    var akanName 
+    validate = (year > 0 && (month >0 && month <=12) && (day > 0 && day <= 31));
     var validateGender = (gender !== "male" && gender !== "female");
-    f (year <= 0 ) {
+    if (year <= 0 ) {
         alert("Invalid year");
     }
     else if (month <= 0 || month > 12) {
