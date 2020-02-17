597eec3b6e505358e944291f8e00749a2aab6464
Edithntinyari <edithtinyari25@gmail.com>
2020-02-17 08:07:43 +0300

    add body

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function giveAkanName () {
     }
     else if (gender === "female" && year > 0 && month > 0 && month < 13 && day > 0 && day < 32){
         akanName = femaleNames[dayBorn];
-    lert("You were born on "+ dayOfTheWeek[dayBorn]+ " and your Akan name is " +akanName);
+    alert("You were born on "+ dayOfTheWeek[dayBorn]+ " and your Akan name is " +akanName);
     }
 }
     function getGender(){
